01e83654f992b1d84efc2844fe40fbc646eed019
AlgoritmikaPos3 <algoritmikaokt@mail.ru>
2024-06-13 17:20:53 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,27 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 5;
+
+    public int level = 1;
+
+    public float speed = 1.2f;
+    void Start()
+    {
+        health += level;
+        print(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
